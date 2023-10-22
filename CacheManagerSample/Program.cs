912b74f980378e0a04679fc3484c4f9ee77fb020
serhiikorol <korol_sergey@me.com>
2023-10-22 16:46:01 +0300

    Initial commit

---
+++ CacheManagerSample/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+using CacheManager.Core;
+
+namespace CacheManagerSample;
+
+static class Program
+{
+    static async Task Main()
+    {
+        var cacheManager = new BaseCacheManager<string>(
+            new ConfigurationBuilder()
+                .WithMicrosoftMemoryCacheHandle()
+                .EnablePerformanceCounters()
+                .EnableStatistics()
+                .WithExpiration(ExpirationMode.Sliding, TimeSpan.FromMinutes(15))
+                .Build()
+        );
+
+        // Add items to the cache
+        cacheManager.Add("item1",
+            DateTime.Now.AddMinutes(1).ToString(CultureInfo.InvariantCulture),
+            "region1");
+        cacheManager.Add("item2",
+            DateTime.Now.AddMinutes(2).ToString(CultureInfo.InvariantCulture),
+            "region1");
+        cacheManager.Add("item1",
+            DateTime.Now.AddMinutes(3).ToString(CultureInfo.InvariantCulture), "region2");
+        var item = new CacheItem<string>(
+            "item2",
+            "region2",
+            DateTime.Now.AddMinutes(4)
+                .ToString(CultureInfo.InvariantCulture),
+            ExpirationMode.Absolute,
+            TimeSpan.FromSeconds(6));
+        cacheManager.Add(item);
+
+        cacheManager.ClearRegion("region1");
+
+        // Start the cache cleanup task (runs in the background)
+        await Task.Run(async () =>
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                DisplayValue(cacheManager, "item1", "region1");
+                DisplayValue(cacheManager, "item2", "region1");
+                DisplayValue(cacheManager, "item1", "region2");
+                DisplayValue(cacheManager, "item2", "region2");
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                cacheManager.Clear();
+                DisplayValue(cacheManager, "item1", "region1");
+                DisplayValue(cacheManager, "item2", "region1");
+                DisplayValue(cacheManager, "item1", "region2");
+                DisplayValue(cacheManager, "item2", "region2");
+            }
+        });
+
+        // Continue with other application tasks
+        Console.WriteLine("Application is running...");
+        Console.ReadLine();
+    }
+    
+    static void DisplayValue(ICacheManager<string> cache, string key, string region)
+    {
+        var value = cache.Get(key, region);
+        Console.WriteLine(value != null
+            ? $"Retrieved value for {key} in {region}: {value}"
+            : $"Item {key} not found in {region}.");
+    }
+}
---
+++ MemoryCache/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Caching.Memory;
+
+var options = new MemoryCacheOptions
+{
+    ExpirationScanFrequency = TimeSpan.FromMinutes(15)
+};
+IMemoryCache cache = new MemoryCache(options);
+cache.Set("item1", "value1", TimeSpan.FromMinutes(1));
+cache.Remove("item1");
